30149cf77219034a2534d3298e6558adf5769d8d
Anna Żaglewska <web.annazaglewska@gmail.com>
2020-11-28 21:36:06 +0100

    adding MainTemplates

---
+++ src/templates/MainTemplate.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../components/navigation/Navbar";
+
+const MainTemplate = ({ children }) => {
+  return (
+    <>
+      <Navbar />
+      {children}
+    </>
+  );
+};
+
+export default MainTemplate;
