7f6793d6318ae74a179c54c6f797e49b0fc2adbc
Anna Żaglewska <web.annazaglewska@gmail.com>
2020-11-28 20:21:55 +0100

    adding new routes and context

--- src/Root.js
+++ src/Root.js
@@ -1,7 +1,29 @@
 import React from "react";
+import { routes } from "./routes";
+// import MainTemplate from "./templates/MainTemplate";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import GlobalStyles from "./GlobalStyles/GlobalStyles";
+import SiteContext from "./context";
 
 const Root = () => {
-  return <div className="App"></div>;
+  return (
+    <>
+      <GlobalStyles />
+      <SiteContext.Provider value={{ products }}>
+        <BrowserRouter>
+          {/* <MainTemplate> */}
+          <Switch>
+            <Route exact path={routes.home} component={Home} />
+            <Route path={routes.about} component={About} />
+            <Route path={routes.collections} component={Collections} />
+            <Route path={routes.gallery} component={Gallery} />
+            <Route path={routes.contact} component={Contact} />
+          </Switch>
+          {/* </MainTemplate> */}
+        </BrowserRouter>
+      </SiteContext.Provider>
+    </>
+  );
 };
 
 export default Root;
